36bfdf5664078157bcc1ccea4382e2703f15ac18
Kris Scott <kscott91@gmail.com>
2024-06-09 17:13:45 -0500

    Remove unused code

--- src/util/error.h
+++ src/util/error.h
@@ -26,12 +26,12 @@ char const* err_string(enum err e);
 #define err_trace(e) err_trace_info(e, __FILE__, __LINE__)
 enum err err_trace_info(enum err e, char const* filename, int lineno);
 
-#define panic(fmt, ...)            \
-    do {                           \
-        log("PANIC", __VA_ARGS__); \
-        abort();                   \
-    } while (0)
+/* #define panic(fmt, ...)            \ */
+/*     do {                           \ */
+/*         log("PANIC", __VA_ARGS__); \ */
+/*         abort();                   \ */
+/*     } while (0) */
 
-#define panic_on_err(e) panic("%s", err_string(e))
+/* #define panic_on_err(e) panic("%s", err_string(e)) */
 
 #endif  // ERROR_H
