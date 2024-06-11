c4f24eee9b1fe261cb15ecba9a26630e57af6ecf
Kris Scott <kscott91@gmail.com>
2024-06-11 12:55:42 -0500

    Add day05b (unverified)

--- src/day05.c
+++ src/day05.c
@@ -1,20 +1,26 @@
 #include "util/error.h"
 #include "util/fileiter.h"
 #include "util/intvec.h"
+#include "util/log.h"
 #include "util/str.h"
 #include "util/vec.h"
 
 static ssize_t const expected_maps = 7;
+
+struct seed {
+    i64 start;
+    i64 len;
+};
+vec_define_struct(seedvec, struct seed);
 
 struct range {
     i64 dest;
     i64 src;
     i64 len;
 };
-
 vec_define_struct(rangevec, struct range);
 
-static enum err parse_seeds(struct intvec *seeds, struct fileiter *f) {
+static enum err parse_seeds(struct seedvec *seeds, struct fileiter *f) {
     enum err e = OK;
     struct str line;
     e = fileiter_line(&line, f);
@@ -23,17 +29,29 @@ static enum err parse_seeds(struct intvec *seeds, struct fileiter *f) {
 
     str_split(&line, line, ":");
 
-    i64 n;
+    struct seed seed = {0};
     for (;;) {
-        switch (e = str_take_int(&n, &line, line)) {
+        switch (e = str_take_int(&seed.start, &line, line)) {
             case OK:
-                vec_push(seeds, n);
                 break;
             case ERR_NONE:
                 return OK;
             default:
                 return err_trace(e);
         }
+        if (PART1) {
+            seed.len = 1;
+        } else {
+            switch (e = str_take_int(&seed.len, &line, line)) {
+                case OK:
+                    break;
+                case ERR_NONE:
+                    return err_trace(ERR_INPUT);
+                default:
+                    return err_trace(e);
+            }
+        }
+        vec_push(seeds, seed);
     }
 }
 
@@ -69,22 +87,101 @@ static enum err parse_ranges(struct rangevec *ranges, struct fileiter *f) {
     }
 }
 
-static void transform_seeds(struct intvec *seeds, struct rangevec const *ranges) {
+static void transform_seeds(struct seedvec *seeds, struct rangevec const *ranges) {
     for (ssize_t i = 0; i < seeds->len; ++i) {
-        i64 seed = seeds->buf[i];
+        struct seed seed = seeds->buf[i];
         for (ssize_t j = 0; j < ranges->len; ++j) {
             struct range const range = ranges->buf[j];
-            if (range.src <= seed && seed < range.src + range.len) {
-                seeds->buf[i] = seed - range.src + range.dest;
+            i64 range_end = range.src + range.len;
+            i64 seed_end = seed.start + seed.len;
+
+            // Split seed range
+            if (range.src > seed.start && range.src < seed_end) {
+                struct seed old_seed = {
+                    .start = seed.start,
+                    .len = range.src - seed.start,
+                };
+                struct seed new_seed = {
+                    .start = range.src,
+                    .len = seed.len - old_seed.len,
+                };
+                log_dbg(
+                    "Split: {%ld,%ld} x {%ld,%ld} -> {%ld,%ld}, {%ld,%ld}",
+                    seed.start,
+                    seed.len,
+                    range.src,
+                    range.len,
+                    old_seed.start,
+                    old_seed.len,
+                    new_seed.start,
+                    new_seed.len
+                );
+                vec_push(seeds, new_seed);
+                seeds->buf[i] = seed = old_seed;
+            } else if (range_end > seed.start && range_end < seed_end) {
+                struct seed old_seed = {
+                    .start = seed.start,
+                    .len = range_end - seed.start,
+                };
+                struct seed new_seed = {
+                    .start = range_end,
+                    .len = seed.len - old_seed.len,
+                };
+                log_dbg(
+                    "Split: {%ld,%ld} x {%ld,%ld} -> {%ld,%ld}, {%ld,%ld}",
+                    seed.start,
+                    seed.len,
+                    range.src,
+                    range.len,
+                    old_seed.start,
+                    old_seed.len,
+                    new_seed.start,
+                    new_seed.len
+                );
+                vec_push(seeds, new_seed);
+                seeds->buf[i] = seed = old_seed;
+            }
+
+            // Transform seed start
+            if (range.src <= seed.start && seed.start < range_end) {
+                struct seed moved_seed = {
+                    .start = seed.start - range.src + range.dest,
+                    .len = seed.len,
+                };
+                log_dbg(
+                    "Move: {%ld,%ld} x {%ld->%ld,%ld} -> {%ld,%ld}",
+                    seed.start,
+                    seed.len,
+                    range.src,
+                    range.dest,
+                    range.len,
+                    moved_seed.start,
+                    moved_seed.len
+                );
+                seeds->buf[i] = moved_seed;
                 break;
             }
         }
     }
 }
 
+static inline void seedvec_log_debug(struct seedvec seeds) {
+#if LOG_DBG
+    struct intvec seed_flat = {0};
+    for (ssize_t i = 0; i < seeds.len; ++i) {
+        vec_push(&seed_flat, seeds.buf[i].start);
+        vec_push(&seed_flat, seeds.buf[i].len);
+    }
+    intvec_log_dbg(seed_flat);
+    vec_deinit(&seed_flat);
+#else
+    (void)seeds;
+#endif
+}
+
 int main(int argc, char *argv[]) {
     struct fileiter f;
-    struct intvec seeds = {0};
+    struct seedvec seeds = {0};
     struct rangevec ranges = {0};
 
     enum err e = fileiter_init_cli(&f, argc, argv);
@@ -93,7 +190,7 @@ int main(int argc, char *argv[]) {
     e = parse_seeds(&seeds, &f);
     if (e) goto error;
 
-    intvec_log_dbg(seeds);
+    seedvec_log_debug(seeds);
 
     ssize_t map_i = 0;
     for (; e != ERR_NONE; ++map_i) {
@@ -102,7 +199,7 @@ int main(int argc, char *argv[]) {
         if (e > ERR_NONE) goto error;
         transform_seeds(&seeds, &ranges);
 
-        intvec_log_dbg(seeds);
+        seedvec_log_debug(seeds);
     }
 
     // Make sure we've used all maps
@@ -111,9 +208,15 @@ int main(int argc, char *argv[]) {
         return ERR_INPUT;
     }
 
-    i64 ans = 0;
-    e = intvec_min(&ans, seeds);
-    printf("%ld\n", ans);
+    if (seeds.len == 0) return err_trace(ERR_INPUT);
+    i64 min = seeds.buf[0].start;
+    for (ssize_t i = 1; i < seeds.len; ++i) {
+        i64 val = seeds.buf[i].start;
+        if (min > val) {
+            min = val;
+        }
+    }
+    printf("%ld\n", min);
 
 error:
     vec_deinit(&ranges);
