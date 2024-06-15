f212d0350b4b4b0100e7a70555da1c87b574b9cc
Kris Scott <kscott91@gmail.com>
2024-06-15 12:43:05 -0500

    Add day07b

--- src/day07.c
+++ src/day07.c
@@ -5,7 +5,7 @@
 #include "util/vec.h"
 
 #define HAND_CARD_COUNT 5
-#define CARD_KIND_COUNT 13
+#define CARD_KIND_COUNT 14
 
 enum strength {
     HIGH_CARD,
@@ -25,20 +25,26 @@ struct hand {
 vec_define_struct(handvec, struct hand);
 
 static int card_kind(char const card) {
+    // Offset kinds by 1 to reserve spot for joker at the front
+
     if ('2' <= card && card <= '9') {
-        return card - '2';
+        return card - '2' + 1;
     }
     switch (card) {
         case 'T':
-            return 10 - 2;
+            return 10 - 1;
         case 'J':
-            return 11 - 2;
+            if (PART1) {
+                return 11 - 1;
+            } else {
+                return 0;
+            }
         case 'Q':
-            return 12 - 2;
+            return 12 - 1;
         case 'K':
-            return 13 - 2;
+            return 13 - 1;
         case 'A':
-            return 14 - 2;
+            return 14 - 1;
         default:
             return -1;
     }
@@ -65,39 +71,13 @@ static int hand_cmp(void const* const left, void const* const right) {
     return 0;
 }
 
-static ERRFN parse_hand(struct hand* const hand, struct str const line) {
-    assert(hand);
-
-    // Split line into cards and bid
-    struct str bid_str;
-    struct str const cards_str = str_split(&bid_str, line, " ");
-
-    // Parse cards
-    if (cards_str.len != HAND_CARD_COUNT) {
-        log_err("Expected %d cards, got: %.*s", HAND_CARD_COUNT, (int)line.len, line.ptr);
-        return ERR_INPUT;
-    }
-
-    char card_counts[CARD_KIND_COUNT] = {0};
-    for (size_t i = 0; i < HAND_CARD_COUNT; ++i) {
-        char const card = cards_str.ptr[i];
-
-        int kind = card_kind(card);
-
-        if (kind < 0) {
-            log_err("Unexpected card '%c' in hand: %.*s", card, (int)line.len, line.ptr);
-            return ERR_INPUT;
-        }
-
-        assert(0 <= kind && kind < CARD_KIND_COUNT);
-        ++card_counts[(size_t)kind];
-        hand->cards[i] = card;
-    }
-
-    // Determine strength
+static enum strength
+get_strength_with_joker(int const card_counts[HAND_CARD_COUNT], int const joker) {
     enum strength strength = HIGH_CARD;
-    for (size_t i = 0; i < CARD_KIND_COUNT; ++i) {
-        switch (card_counts[i]) {
+
+    for (size_t i = PART1 ? 0 : 1; i < CARD_KIND_COUNT; ++i) {
+        int joker_count = (!PART1 && joker == (int)i) ? card_counts[0] : 0;
+        switch (card_counts[i] + joker_count) {
             case 0:
             case 1:
                 break;
@@ -150,10 +130,66 @@ static ERRFN parse_hand(struct hand* const hand, struct str const line) {
                 assert(0);
         }
     }
-    log_dbg("Strength: %d", strength);
-    hand->strength = strength;
+
+    return strength;
+}
+
+static enum strength get_strength(int const card_counts[HAND_CARD_COUNT]) {
+    if (PART1 || card_counts[0] == 0) {
+        enum strength const strength = get_strength_with_joker(card_counts, -1);
+        log_dbg("Strength: %d", strength);
+        return strength;
+    } else {
+        // TODO: Optimize based on max possible from non-joker score and joker count
+
+        enum strength max_strength = HIGH_CARD;
+        for (int joker = 1; joker < CARD_KIND_COUNT; ++joker) {
+            enum strength strength = get_strength_with_joker(card_counts, joker);
+            if (max_strength < strength) {
+                max_strength = strength;
+            }
+        }
+        log_dbg("Strength: %d", max_strength);
+        return max_strength;
+    }
+}
+
+static ERRFN parse_hand(struct hand* const hand, struct str const line) {
+    assert(hand);
+
+    // Split line into cards and bid
+    struct str bid_str;
+    struct str const cards_str = str_split(&bid_str, line, " ");
+
+    // Parse cards
+
+    if (cards_str.len != HAND_CARD_COUNT) {
+        log_err("Expected %d cards, got: %.*s", HAND_CARD_COUNT, (int)line.len, line.ptr);
+        return ERR_INPUT;
+    }
+
+    int card_counts[CARD_KIND_COUNT] = {0};
+    for (size_t i = 0; i < HAND_CARD_COUNT; ++i) {
+        char const card = cards_str.ptr[i];
+
+        int kind = card_kind(card);
+
+        if (kind < 0) {
+            log_err("Unexpected card '%c' in hand: %.*s", card, (int)line.len, line.ptr);
+            return ERR_INPUT;
+        }
+
+        assert(0 <= kind && kind < CARD_KIND_COUNT);
+        ++card_counts[(size_t)kind];
+        hand->cards[i] = card;
+    }
+
+    // Determine strength
+
+    hand->strength = get_strength(card_counts);
 
     // Parse bid
+
     enum err const e = str_take_int(&hand->bid, NULL, bid_str);
     if (e == ERR_NONE) {
         log_err("Could not parse bid: %.*s", (int)line.len, line.ptr);
